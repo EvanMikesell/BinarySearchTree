12db24b7281d68f29c2492b9f58c973ef626ecb9
Evan Mikesell <mikesellevan@gmail.com>
2022-10-11 14:49:11 -0700

    added beginning of strategy pattern

--- bst.py
+++ bst.py
@@ -1,4 +1,8 @@
+from xmlrpc.client import Boolean
+
+
 class BinarySearchTree:
+    # strategy = GPAOrder() TO do now : figure out how to do the strategy method
     def __init__(self):
         self._root = self.NullNode()
 
@@ -35,13 +39,23 @@ class BinarySearchTree:
         def toString(self):
             return self._last_name, self._first_name, self._gpa, self._red_id
 
+        def get_gpa(self):
+            return self._gpa
+
+        def get_last_name(self):
+            return self._last_name
+
+        def get_first_name(self):
+            return self._first_name
+
         def for_each(self, lambda_arg):
-            # lambda_arg(current_node)
-            print(self._last_name)
+            lambda_arg(self)
             self._left_child.for_each(lambda_arg)
             self._right_child.for_each(lambda_arg)
 
         def insert(self, new_node, parent_node):
+
+            # replace this comparison with strategy comparison method
             if new_node._gpa < self._gpa:
                 self._left_child = self._left_child.insert(
                     new_node, self._left_child)
@@ -62,17 +76,53 @@ class BinarySearchTree:
             return 'NULL NODE'
 
 
+class Strategy:
+    def greater_than(node1, node2):
+        pass
+
+
+class GPAOrder(Strategy):
+    def greater_than(self, node1, node2) -> bool:
+        if node1.get_gpa() > node2.get_gpa():
+            return True
+        else:
+            return False
+
+
+class NameOrder(Strategy):
+    def greater_than(self, node1, node2) -> bool:
+        # x<y means "does x come before y alphabetically?"
+        if node1.get_last_name() == node2.get_last_name():
+            if node1.get_first_name() < node1.get_first_name():
+                return True
+            else:
+                return False
+
+        elif node1._last_name < node2._last_name:
+            return True
+        else:
+            return False
+
+
 tree = BinarySearchTree()
 tree.insert('Mikesell', 'Evan', 820215988, 4.0)
-tree.insert('A', 'B', 9202, 3.2)
+tree.insert('Mikesell', 'B', 9202, 3.2)
 tree.insert('lastname', 'firstname', 9202, 3.4)
+
+
+def x(a): return print(a._last_name)
+# tree.for_each(x)
+
+
+name_strategy = NameOrder()
+gpa_strategy = GPAOrder()
+print("two nodes:")
 print(tree._root.toString())
 print(tree._root._left_child.toString())
-print(tree._root._left_child._right_child.toString())
-print(tree._root._right_child.toString())
+print(name_strategy.greater_than(tree._root, tree._root._left_child))  # false
+print(gpa_strategy.greater_than(tree._root, tree._root._left_child))  # true
 
 
-tree.for_each(10)
 """
 insert
     method in BST
